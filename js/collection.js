ee46c279b280b83a0824e2c343dd20c8c4fc5e84
michal.duris <michal.duris7@gmail.com>
2021-03-07 22:16:04 +0100

    #9 Part 2 - Added javascript skeleton for creation of the alphabet navigation bar programmatically

---
+++ js/collection.js
@@ -0,0 +1,39 @@
+/**
+ * Generate alphabet
+ *
+ * @returns {*[]} array containing alphabet letters
+ */
+function generateAlphabet() {
+    let alphabet = [];
+
+    // TODO Implement alphabet generation. You can be inspired by the internet. They are lots of examples available.
+
+    return alphabet;
+}
+
+/**
+ * Create letter button.
+ *
+ * @param text text to be displayed
+ * @return newly created letter button
+ */
+function createLetterButton(text) {
+    const button = null;
+    // TODO 1. Create element
+    //      2. Add classes btn, btn-light and starting-letter-criteria
+    //      3. Set button type as button
+    //      3. Add the letter param as a text
+    return button;
+}
+
+/**
+ * Initialize container by generating and appending alphabet criteria buttons.
+ */
+function initAlphabetContainer() {
+    // TODO 1. Call #generateAlphabet
+    //      2. For each element generate a letter button by calling #createLetterButton
+    //      3. Append created buttons into #alphabet-container
+    //      4. Create another letter button with text 'Other' and append it also into #alphabet-container
+}
+
+// TODO Call here #initAlphabetContainer
