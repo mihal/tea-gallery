180fabc7a8244caedf64e7fb7765f9ef0c82fd9c
michal.duris <michal.duris7@gmail.com>
2021-03-12 18:42:20 +0100

    9.5 - Introduction into classes and better decomposition

--- js/collection.js
+++ js/collection.js
@@ -1,41 +1,60 @@
-/**
- * Generate alphabet
- *
- * @returns {*[]} array containing alphabet letters
- */
-function generateAlphabet() {
-    let alphabet = [];
+// TODO A: 1. Read 'Read & delete TODO'
+//         2. Create here class CollectionPageOperator
+//         3. Create private method init
+//         4. Move function initAlphabetContainer inside a CollectionPageOperator (as a private method)
+//         5. Call initAlphabetContainer from init method
+//         6. Expose method named init, which will be calling the private init method
 
-    const start = 'A'.charCodeAt(0);
-    const last  = 'Z'.charCodeAt(0);
-    for (let i = start; i <= last; ++i) {
-        alphabet.push(String.fromCharCode(i));
+// TODO Read & delete: This is 'old' JS definition of the class. Objects of a class can be created with new, e.g. new AlphabetProvider().
+function AlphabetProvider() {
+    // TODO Read & delete: This is a private method (function) of the class. It will not be accessible outside this class implicitly.
+    /**
+     * Generate alphabet
+     *
+     * @returns {*[]} array containing alphabet letters
+     */
+    function generateAlphabet() {
+        let alphabet = [];
+
+        const start = 'A'.charCodeAt(0);
+        const last = 'Z'.charCodeAt(0);
+        for (let i = start; i <= last; ++i) {
+            alphabet.push(String.fromCharCode(i));
+        }
+
+        return alphabet;
     }
 
-    return alphabet;
-}
+    /**
+     * Create letter button.
+     *
+     * @param text text to be displayed
+     * @return newly created letter button
+     */
+    function createLetterButton(text) {
+        const button = document.createElement("button");
 
-/**
- * Create letter button.
- *
- * @param text text to be displayed
- * @return newly created letter button
- */
-function createLetterButton(text) {
-    const button = document.createElement("button");
+        button.classList.add("btn", "btn-light", "starting-letter-criteria", "m-1");
+        button.type = "button";
+        button.innerText = text;
 
-    button.classList.add("btn", "btn-light", "starting-letter-criteria", "m-1");
-    button.type = "button";
-    button.innerText = text;
+        return button;
+    }
 
-    return button;
+    // TODO Read & delete: Here you are defining which methods (functions) should be public, i.e. visible outside this class.
+    //      This is a definition of the API of this class. The class AlphabetProvider exposes two methods named: generateAlphabet and createLetterButton
+    //      In this case method name is identical with the function name.
+    return {
+        generateAlphabet: generateAlphabet,
+        createLetterButton: createLetterButton
+    }
 }
 
 /**
  * Initialize container by generating and appending alphabet criteria buttons.
  */
 function initAlphabetContainer() {
-
+    // TODO B: We have a problem here. generateAlphabet and createLetterButton are not available. How we can fix it?
     let alphabet = generateAlphabet();
     const alphabetContainer = document.getElementById("alphabet-container");
 
@@ -44,7 +63,6 @@ function initAlphabetContainer() {
     });
 
     alphabetContainer.append(createLetterButton("Other"));
-
 }
 
-initAlphabetContainer();
+// TODO C: Create an instance (object) of the class CollectionPageOperator and call init
