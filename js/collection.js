b95d088681455e2e305f7455992dca01bbe430ef
michal.duris <michal.duris7@gmail.com>
2021-03-28 18:53:57 +0200

    T-2 Tea items loading - 1st phase

--- js/collection.js
+++ js/collection.js
@@ -38,6 +38,28 @@ function AlphabetProvider() {
     }
 }
 
+function ViewSpinnerOperator() {
+
+    const CONST = {
+        TEMPLATE: 'items-view-spinner-template',
+        ACTIVE: 'items-view-spinner'
+    }
+
+    return {
+        show: function (viewContainer) {
+            // TODO 1. Find template by id. Value is stored in CONST.TEMPLATE.
+            //      2. Create a clone
+            //      3. Change id to active one. (CONST.ACTIVE)
+            //      4. Set the cloned element into the viewContainer.
+            //      5. Target state is that the spinner is solo in the view section. No companies are displayed.
+        },
+        hide: function () {
+            // TODO 1. Find element by the id. Value is stored in CONST.ACTIVE.
+            //      2. Remove the element.
+        }
+    }
+}
+
 function CollectionPageOperator() {
 
     let handler = new CompaniesHandler();
@@ -45,15 +67,8 @@ function CollectionPageOperator() {
     function init() {
         initAlphabetContainer();
         initItemsView();
-    }
 
-    function initItemsView() {
-        const companyNames = handler.loadItems();
-        const itemsView = document.getElementById('items-view');
-
-        companyNames.forEach(companyName => {
-            itemsView.append(handler.createElementFor(companyName));
-        });
+        registerCompanyCardClickEvents();
     }
 
     /**
@@ -69,6 +84,34 @@ function CollectionPageOperator() {
         });
 
         alphabetContainer.append(alphabetProvider.createLetterButton("Other"));
+    }
+
+    function initItemsView() {
+        // TODO Before you call loadItems, show loading spinner. Use ViewSpinnerOperator class and itemsView. (Rearrangement needed)
+        const companyNames = handler.loadItems();
+        // TODO After the loading is finished. Hide the spinner and start rendering.
+        const itemsView = document.getElementById('items-view');
+
+        companyNames.forEach(companyName => {
+            const itemContainer = document.createElement('div');
+            itemContainer.classList.add('col');
+            itemContainer.append(handler.createElementFor(companyName));
+
+            itemsView.append(itemContainer);
+        });
+    }
+
+    function registerCompanyCardClickEvents() {
+        // TODO Add click event handler on all .company-card elements. Callback function is handleCompanyCardClicked
+    }
+
+    function handleCompanyCardClicked(event) {
+        // TODO 1. Retrieve a clicked element from the event. You either google or investigate content of the event object printing it by console.log(event)
+        //      2. Find the nearest (closest) ancestor mark as .company-card. We need to cover case when you click on .company-name or other children.
+        //      3. Retrieve from the company-card element the name of the company. (It is stored inside a data attribute set in companiesHandler.js)
+        //      4. Find a currently active breadcrumb and remove the active class. (Find by a selector of two classes)
+        //      5. Find #company-breadcrumb-item. Set the company name, mark with active class and remove invisible.
+        //      6. Show view spinner.
     }
 
     return {
--- js/companiesHandler.js
+++ js/companiesHandler.js
@@ -5,7 +5,7 @@ function CompaniesHandler() {
          *
          * @return {String[]} available company names
          */
-        loadItems: function() {
+        loadItems: function () {
             return ["Teekanne", "Pickwick", "Pukka"];
         },
         /**
@@ -14,9 +14,10 @@ function CompaniesHandler() {
          * @param companyData input data, the company HTML element will be created from
          * @return {Node} HTML representation of the company
          */
-        createElementFor: function(companyData) {
+        createElementFor: function (companyData) {
             let companyCard = document.getElementById('company-template').cloneNode(true);
             companyCard.removeAttribute('id');
+            // TODO Add an attribute called data-name into companyCard. Value is companyData.
             companyCard.querySelector('.company-name').innerText = companyData;
 
             return companyCard;
